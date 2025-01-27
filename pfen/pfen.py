a8cc80e674b2652a6634e23ec5328e2bd4aeb3c7
Alex <alex.q.fredrickson@gmail.com>
2025-01-27 13:35:22 -0500

    expanded pfen

--- pfen/pfen.py
+++ pfen/pfen.py
@@ -2,27 +2,48 @@ class PFEN:
     """
     PFEN is a string-notation that describes a position in a Patchwork game. For example:
 
-    `PyPjPbPpPccPlPffPggPbbPaaPcPfPzPgPuPrPmPddPnPePsPvPtPeePaPxPiPkPoPdPqPhPw 1. Pbc1Pxf4 Pbc1Pxf4H ...`
+    `PyPjPbPpPccPlPffPggPbbPaaPcPfPzPgPuPrPmPddPnPePsPvPtPeePaPxPiPkPoPdPqPhPw 1. PbR0F0c1Pxf4 PcR0F0c1Pdf4H ...`
 
     Game Concepts
     -------------
-    - Each game has a "starting position", which refers to the ordering of the 33 randomized starting patches.
-    - Players place one or more of these patches until they have advanced ahead of their opponent.
-    - Players occasionally obtain and place 1x1 squares by virtue of advancing.
-    - Players occasionally hop their opponent.
+    ⦾ Each game has a "starting position", which refers to the ordering of the 33 randomized starting patches.
+    ⦾ Players place one or more of these patches until they have advanced ahead of their opponent.
+    ⦾ Players occasionally obtain and place 1x1 squares by virtue of advancing.
+    ⦾ Players occasionally hop their opponent.
 
-    PFEN Concepts
+    Starting Position
     -------------
-    - Each "patch" is given an arbitrary name. Each leads with a capital `P` - for example: `Pff`.
-    - 1x1 squares are uniformly named `Phh`.
-    - The PFEN starts with a *starting position string*, which represents each piece, in (cyclical) order. This string
-      terminates with a space
-    - `1. ` denotes the first *move sequence* of each player.
-    - A *move sequence* may contain *piece placements*, and/or *hops*.
-    - A *hop* is denoted by a capital `H`, and may result in a 1x1 piece placement at some position. For example:
-      `HPhhd5`.
+    ⦾ Each patch is given a unique arbitrary name, leading with `P`. For example: `Pff`.
+        ⦾ 1x1 squares are all called `Phh`.
+    ⦾ The PFEN begins with a sequence of patch names representing the starting position.
+        ⦾ It terminates with a single whitespace.
 
-      # todo: explain moves in greater detail
+    Turns
+    -----
+    ⦾ Each turn is denoted by an integer. For example: `1. `.
+    ⦾ Each turn has two *move sequences*.
+
+    Move Sequences
+    --------------
+    ⦾ A player's move sequence consists of zero or more *piece placements*, and may end with a *hop*.
+
+    Piece Placements
+    ----------------
+    ⦾ In the *piece placement* section, each patch is described with its rotations and flips, in a certain location.
+        ⦾ Rotations are denoted by `R0`, `R1`, `R2`, or `R3`, depending on whether or not the patch is rotated 0, 90,
+          180, or 270 degrees clockwise (respectively).
+        ⦾ Flips are denoted by `F0` or `F1`, depending on whether or not the patch is flipped along its first axis
+          (according to np.flip(axis=1)).
+        ⦾ Placements are described in algebraic notation, where A1 refers to the top-left quadrant, and I9 refers to
+          the bottom-right quadrant.
+          ⦾ Placements are described as if the piece occupies its own NxM board, where N is the piece height and M
+            is the piece width.
+
+    ⦾ For example: PbR1F1c1 means the `Pb` piece is flipped, and then rotated 90 degrees clockwise, and then placed at
+      `c1` (as if the piece was a rectangle).
+
+    ⦾ A *hop* is denoted by a capital `H`, and may result in a 1x1 piece placement at some position. For example:
+      `HPhhd5`.
 
     Other Notes
     -----------
